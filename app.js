4923dde80f37791a8c9f2684521e8e0bdbb92011
Andrew Bautista <andrewbautista85@gmail.com>
2020-03-06 21:47:09 -0500

    api displaying correctly in api/notes

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const PORT = 8080;
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "public/notes.html")));
 
-app.get("*", (req, res) => res.sendFile(path.join(__dirname, "public/index.html")));
-
 app.get("/api/notes", (req, res) => {
 
+    const db = fs.readFile("./db/db.json", "utf8", (err, data) => {
+        if (err) {
+            throw err;
+        } else {
+            const obj = JSON.parse(data)
+            return res.json(obj);
+
+        }
+    });
+
+    return db;
+
 })
+
+app.get("*", (req, res) => res.sendFile(path.join(__dirname, "public/index.html")));
 
 
 app.listen(PORT, function () {
