62601aafdc01f31a984d4e4e0b2210f24170ee22
Andrew Bautista <andrewbautista85@gmail.com>
2020-03-09 17:58:36 -0400

    turned read file into a function

--- app.js
+++ app.js
@@ -1,28 +1,43 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const util = require('util');
 
 const app = express();
 const PORT = 8080;
 
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
+getJSON = () => {
+
+    return readFileAsync("./db/db.json", "utf8").then(data => {
+        console.log(data);
+        return JSON.parse(data);
+    });
+
+}
+
 app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "public/notes.html")));
 
-app.get("/api/notes", (req, res) => {
+app.get("/api/notes", async (req, res) => {
 
-    const db = fs.readFile("./db/db.json", "utf8", (err, data) => {
-        if (err) {
-            throw err;
-        } else {
-            const obj = JSON.parse(data)
-            return res.json(obj);
+    const json = await getJSON();
 
-        }
-    });
+    res.json(json);
+
+
+
+
+});
+
+app.post("api/notes", (req, res) => {
+
+
 
 })
 
