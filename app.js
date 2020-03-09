d64f4a51fb3aef6a520311c0c0c31872a18fa3fa
Andrew Bautista <andrewbautista85@gmail.com>
2020-03-09 17:14:01 -0400

    added use static

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const PORT = 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "public/notes.html")));
 
