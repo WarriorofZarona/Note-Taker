bf9ecd491fed2bbeeb930ac4ca88500de5d97afc
Andrew Bautista <andrewbautista85@gmail.com>
2020-03-09 19:56:21 -0400

    cleaned code, first note in id will start at 1

--- app.js
+++ app.js
@@ -13,22 +13,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-getJSON = () => {
-
+getJSON = () => { // reads the db.json file
     return readFileAsync("./db/db.json", "utf8").then(data => {
         return JSON.parse(data);
     });
-
 };
 
-writetoFile = notes => {
+writetoFile = notes => { //updates the db.json file
     writeFileAsync("./db/db.json", JSON.stringify(notes)).then(() => console.log("Successfully wrote to db.json!"))
-
 }
 
-mapId = (arr) => {
+mapId = (arr) => { //maps an ID based on index
     return arr.map((val, index) => {
-        val.id = index;
+        val.id = index + 1;
         return val;
     });
 }
@@ -46,10 +43,8 @@ app.post("/api/notes", async (req, res) => {
 
     const noteArr = await getJSON();
     const newNote = req.body;
-    console.log(`Adding the following note: ${newNote}`)
     noteArr.push(newNote);
     const noteId = mapId(noteArr);
-    console.log(`Updated notes: ${noteArr}`)
     writetoFile(noteArr);
     return res.json(newNote);
 });
@@ -57,17 +52,15 @@ app.post("/api/notes", async (req, res) => {
 app.delete("/api/notes/:id", async (req, res) => {
 
     const noteArr = await getJSON();
-    console.log(req.params.id);
     const deleteId = req.params.id;
-    console.log(`Chose to delete Note ID: ${deleteId}`);
 
     for (let i = 0; i < noteArr.length; i++) {
         if (deleteId == noteArr[i].id) {
-            console.log("Removing Note ID " + noteArr[i].id)
+
             noteArr.splice(i, 1);
+
         };
     }
-    console.log(noteArr)
     mapId(noteArr);
     writetoFile(noteArr);
     res.json(noteArr);
